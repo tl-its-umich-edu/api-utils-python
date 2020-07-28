4040702936540285630b5e3b041996dd62d36329
Sam Sciolla <ssciolla@umich.edu>
2020-07-28 14:40:09 -0400

    Use and extend ranges for autologging, python-dotenv, requests

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
     package_data={'umich_api': ['apis.json']},
     install_requires=[
         'ratelimit>=2.2.0,<2.2.99',
-        'requests>=2.20.0,<2.22.99',
-        'python-dotenv==0.9.1',
-        'autologging==1.2',
+        'requests>=2.20.0,<2.24.99',
+        'python-dotenv>=0.9.1,<0.14.99',
+        'autologging>=1.2.0,<1.3.99',
     ],
     setup_requires=['setuptools_scm',],
     tests_require=[],
